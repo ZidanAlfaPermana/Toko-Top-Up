4c97230e7228cbb7195fb5bb958bfc25e6c8f6f7
ZidanAlfaPermana <zidanalfa54r@gmail.com>
2026-07-18 19:52:51 +0700

    added some important tables and fixing database id

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Category extends Model
 {
     use HasUlids;
-    protected $fillable = ['name', 'category', 'price'];
 }
